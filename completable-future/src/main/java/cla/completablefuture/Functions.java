3dd6855e53dd4fcf7611e63ddd73ea6800e853dd
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2016-03-17 00:07:45 +0100

    curry

---
+++ completable-future/src/main/java/cla/completablefuture/Functions.java
@@ -0,0 +1,15 @@
+package cla.completablefuture;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Functions {
+
+    public static <A1, A2, R> Function<
+        BiFunction<A1, A2, R>,
+        Function<A2, R>
+    > curry(A1 arg1) {
+        return uncurried -> arg2 -> uncurried.apply(arg1, arg2);
+    }
+
+}
--- quasar-cf/src/test/java/cla/completablefuture/nonblocking/callback/QuasarCallbackJenkinsPluginTest.java
+++ quasar-cf/src/test/java/cla/completablefuture/nonblocking/callback/QuasarCallbackJenkinsPluginTest.java
@@ -1,10 +1,9 @@
 package cla.completablefuture.nonblocking.callback;
 
 import cla.completablefuture.ConsolePlusFile;
+import cla.completablefuture.Functions;
 import cla.completablefuture.jenkins.AsyncJenkinsPlugin;
 import cla.completablefuture.jenkins.JenkinsPlugin;
-import cla.completablefuture.jenkins.blocking.JenkinsPlugin_Collect;
-import cla.completablefuture.jenkins.nonblocking.JenkinsPlugin_Collect_Quasar;
 import cla.completablefuture.jenkins.nonblocking.callback.JenkinsPlugin_CallbackCollect_Quasar;
 import cla.completablefuture.jira.JiraBundle;
 import cla.completablefuture.jira.JiraComponent;
@@ -12,7 +11,6 @@ import cla.completablefuture.jira.JiraServerException;
 import cla.completablefuture.jira.nonblocking.FakeJiraServer;
 import cla.completablefuture.jira.nonblocking.JiraServer;
 import cla.completablefuture.jira.nonblocking.JiraServerWithLatency;
-import cla.completablefuture.jira.nonblocking.callback.Callback;
 import cla.completablefuture.jira.nonblocking.callback.CallbackJiraServer;
 import com.jasongoodwin.monads.Try;
 import org.junit.FixMethodOrder;
@@ -26,18 +24,17 @@ import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.BiFunction;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
+import static cla.completablefuture.Functions.curry;
 import static cla.completablefuture.jira.nonblocking.FakeJiraServer.NB_OF_BUNDLES_PER_NAME;
 import static cla.completablefuture.jira.nonblocking.FakeJiraServer.NB_OF_COMPONENTS_PER_BUNDLE;
 import static java.lang.Runtime.getRuntime;
 import static java.lang.System.out;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.singleton;
-import static java.util.concurrent.CompletableFuture.completedFuture;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.concurrent.ForkJoinPool.commonPool;
@@ -180,7 +177,7 @@ public class QuasarCallbackJenkinsPluginTest {
             cla.completablefuture.jenkins.nonblocking.JenkinsPlugin_Collect_Quasar::new
         );
         List<BiFunction<CallbackJiraServer, Executor, JenkinsPlugin>> callbackNonBlockingPlugins = Arrays.asList(
-            cla.completablefuture.jenkins.nonblocking.callback.JenkinsPlugin_CallbackCollect_Quasar::new
+                cla.completablefuture.jenkins.nonblocking.callback.JenkinsPlugin_CallbackCollect_Quasar::new
         );
 
         cla.completablefuture.jira.blocking.JiraServer blockingSrv = new cla.completablefuture.jira.blocking.JiraServerWithLatency(new cla.completablefuture.jira.blocking.FakeJiraServer());
@@ -188,18 +185,9 @@ public class QuasarCallbackJenkinsPluginTest {
         CallbackJiraServer callbackNonBlockingSrv = new CallbackJiraServerWithLatency(new FakeCallbackJiraServer());
 
         List<Function<Executor,JenkinsPlugin>> allPlugins = new ArrayList<>();
-        allPlugins.addAll(blockingPlugins.stream().map(pluginPrototype -> {
-            Function<Executor, JenkinsPlugin> pool2Plugin = pool -> pluginPrototype.apply(blockingSrv, pool);
-            return pool2Plugin;
-        }).collect(toList()));
-        allPlugins.addAll(nonBlockingPlugins.stream().map(pluginPrototype -> {
-            Function<Executor, JenkinsPlugin> pool2Plugin = pool -> pluginPrototype.apply(nonBlockingSrv, pool);
-            return pool2Plugin;
-        }).collect(toList()));
-        allPlugins.addAll(callbackNonBlockingPlugins.stream().map(pluginPrototype -> {
-            Function<Executor, JenkinsPlugin> pool2Plugin = pool -> pluginPrototype.apply(callbackNonBlockingSrv, pool);
-            return pool2Plugin;
-        }).collect(toList()));
+        allPlugins.addAll(blockingPlugins.stream().map(curry(blockingSrv)).collect(toList()));
+        allPlugins.addAll(nonBlockingPlugins.stream().map(curry(nonBlockingSrv)).collect(toList()));
+        allPlugins.addAll(callbackNonBlockingPlugins.stream().map(curry(callbackNonBlockingSrv)).collect(toList()));
         return allPlugins;
     }
 
